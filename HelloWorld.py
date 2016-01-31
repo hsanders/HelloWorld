7155564242510a295489b5775708bbeecc206511
hsanders <hsand2@uis.edu>
2016-01-31 09:53:15 -0600

    Initial Commit

---
+++ HelloWorld.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 28 13:43:02 2016
+
+@author: Heather Sanders
+
+"""
+def HelloWorld():
+    # Print "Hello World!" and return None
+    print "Hello World!"
+
+# main program
+HelloWorld()
---
+++ README.txt
@@ -0,0 +1,6 @@
+Heather R. Sanders
+CSC 570: Data Science Essentials
+Mike Bernico
+January 28, 2016
+
+HelloWorld Python Program for Lab 2: Set up Git
